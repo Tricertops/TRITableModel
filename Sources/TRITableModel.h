dc10ae57de5d701e84e1935d94b2a150223bc083
Martin Kiss <martin.kiss@me.com>
2015-01-19 11:58:39 +0100

    Project, settings, go!

---
+++ Sources/TRITableModel.h
@@ -0,0 +1,19 @@
+//
+//  TRITableModel.h
+//  TRITableModel
+//
+//  Created by Martin Kiss on 19.1.15.
+//  Copyright (c) 2015 Triceratops. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TRITableModel.
+FOUNDATION_EXPORT double TRITableModelVersionNumber;
+
+//! Project version string for TRITableModel.
+FOUNDATION_EXPORT const unsigned char TRITableModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TRITableModel/PublicHeader.h>
+
+
